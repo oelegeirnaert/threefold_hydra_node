d14aec3212386b818388d399f4d7f1dc05ddb4e8
Oele Geirnaert <oelegeirnaert@hotmail.com>
2022-05-11 14:51:12 +0200

    wip

--- src/webapp/helpers/hydra.py
+++ src/webapp/helpers/hydra.py
@@ -7,28 +7,12 @@ import json
 def get_cli_info(hydra_cli_argument):
     try:
         p = (
-            subprocess.check_output("hydra-cli -{0}".format(hydra_cli_argument).split())
+            subprocess.check_output("hydra-cli {0}".format(hydra_cli_argument).split())
             .decode("utf8")
             .replace("'", '"')
         )
         data = json.loads(p)
         s = json.dumps(data, indent=4, sort_keys=True)
         return s
-    except Exception as e:
-        return "Hydra Application not found."
-
-
-def get_wallet_info():
-    try:
-        p = subprocess.Popen("hydra-cli -getwalletinfo", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        return p.communicate()
-    except Exception as e:
-        return "Hydra Application not found."
-
-
-def get_staking_info():
-    try:
-        p = subprocess.Popen("hydra-cli -getstakinginfo", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        return p.communicate()
     except Exception as e:
         return "Hydra Application not found."
