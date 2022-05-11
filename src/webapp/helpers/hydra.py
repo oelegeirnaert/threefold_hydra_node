9ed6d3d1b7bbf669258c58e6a0cfecf53cd5b4d2
Oele Geirnaert <oelegeirnaert@hotmail.com>
2022-05-11 14:40:07 +0200

    format

--- src/webapp/helpers/hydra.py
+++ src/webapp/helpers/hydra.py
@@ -1,12 +1,19 @@
 import os
 import socket
 import subprocess
+import json
 
 
-def get_hydra_info():
+def get_cli_info(hydra_cli_argument):
     try:
-        p = subprocess.check_output("hydra-cli -getinfo".split())
-        return p
+        p = (
+            subprocess.check_output("hydra-cli -{0}".format(hydra_cli_argument).split())
+            .decode("utf8")
+            .replace("'", '"')
+        )
+        data = json.loads(p)
+        s = json.dumps(data, indent=4, sort_keys=True)
+        return s
     except Exception as e:
         return "Hydra Application not found."
 
